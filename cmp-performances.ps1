d2b2d7de5d80821e3932fb421f040a449b4f7563
InWave <jinzuo75@163.com>
2024-04-12 16:58:13 +0800

    add two hd decoder

--- cmp-performances.ps1
+++ cmp-performances.ps1
@@ -27,3 +27,48 @@ for ($i = 1; $i -le $loopNum; $i++) {
 }
 $execAgvTime = $totalTime / $loopNum
 Write-Host "software Decoder average execution time: $execAgvTime"
+
+# 使用dxva2解码的方式
+$totalTime = 0
+for ($i = 1; $i -le $loopNum; $i++) {
+    #rm C:\Users\zuo.jin\qsv-output.yuv
+    $startTime = Get-Date
+    # 执行你的命令  -loglevel verbose
+    #ffmpeg -hide_banner -c:v h264 -i .\output.mp4 -c:v rawvideo -pix_fmt yuv420p sft-output.yuv
+    ffmpeg -loglevel quiet -hide_banner -hwaccel dxva2 -c:v h264 -i .\output.mp4 -f null -
+    $endTime = Get-Date
+    $executionTime = $endTime - $startTime
+    $totalTime = $executionTime + $totalTime
+}
+$execAgvTime = $totalTime / $loopNum
+Write-Host "DXVA2 Decoder average execution time: $execAgvTime"
+
+# 使用opencl解码的方式，怀疑这里走的就是软件解码，只是通过opencl来调用而已
+$totalTime = 0
+for ($i = 1; $i -le $loopNum; $i++) {
+    #rm C:\Users\zuo.jin\qsv-output.yuv
+    $startTime = Get-Date
+    # 执行你的命令  -loglevel verbose
+    #ffmpeg -hide_banner -c:v h264 -i .\output.mp4 -c:v rawvideo -pix_fmt yuv420p sft-output.yuv
+    ffmpeg -loglevel quiet -hide_banner  -hwaccel_device opencl -c:v h264 -i .\output.mp4 -f null -
+    $endTime = Get-Date
+    $executionTime = $endTime - $startTime
+    $totalTime = $executionTime + $totalTime
+}
+$execAgvTime = $totalTime / $loopNum
+Write-Host "OpenCL Decoder average execution time: $execAgvTime"
+
+# 使用d3d11va解码的方式
+$totalTime = 0
+for ($i = 1; $i -le $loopNum; $i++) {
+    #rm C:\Users\zuo.jin\qsv-output.yuv
+    $startTime = Get-Date
+    # 执行你的命令  -loglevel verbose
+    #ffmpeg -hide_banner -c:v h264 -i .\output.mp4 -c:v rawvideo -pix_fmt yuv420p sft-output.yuv
+    ffmpeg -loglevel quiet -hide_banner -hwaccel d3d11va -c:v h264 -i .\output.mp4 -f null -
+    $endTime = Get-Date
+    $executionTime = $endTime - $startTime
+    $totalTime = $executionTime + $totalTime
+}
+$execAgvTime = $totalTime / $loopNum
+Write-Host "d3d11va Decoder average execution time: $execAgvTime"
